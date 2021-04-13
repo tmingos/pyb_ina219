1b031fbe0c16c3f292cb37565c20c02256979aa0
tmingos <54285+tmingos@users.noreply.github.com>
2021-04-13 10:37:22 -0400

    Update readme and expample

--- esp8266/example.py
+++ esp8266/example.py
@@ -1,3 +1,5 @@
+# With Logging (Regular Driver)
+
 from machine import Pin, I2C
 from ina219 import INA219
 from logging import INFO
@@ -6,6 +8,22 @@ SHUNT_OHMS = 0.1
 
 i2c = I2C(-1, Pin(5), Pin(4))
 ina = INA219(SHUNT_OHMS, i2c, log_level=INFO)
+ina.configure()
+
+print("Bus Voltage: %.3f V" % ina.voltage())
+print("Current: %.3f mA" % ina.current())
+print("Power: %.3f mW" % ina.power())
+
+
+# Without Logging (ESP8266)
+
+from machine import Pin, I2C
+from ina219_no_logging import INA219
+
+SHUNT_OHMS = 0.1
+
+i2c = I2C(-1, Pin(5), Pin(4))
+ina = INA219(SHUNT_OHMS, i2c)
 ina.configure()
 
 print("Bus Voltage: %.3f V" % ina.voltage())
